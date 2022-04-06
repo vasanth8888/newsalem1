7b40b1b8ff45c5af23c5f7e14deebfdd8e1c842c
vasanth8888 <vasanthveeda@gmail.com>
2022-04-06 22:01:37 +0530

    newsalem

---
+++ src/test/java/org/pack/Newsalem.java
@@ -0,0 +1,32 @@
+package org.pack;
+
+public class Newsalem {
+	
+	        private void test1() {
+				// TODO Auto-generated method stub
+
+			}
+	        private void test2() {
+				// TODO Auto-generated method stub
+
+			}
+	        
+	  private void test3() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
